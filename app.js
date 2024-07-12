ef9a902356c9559edaab3e6e1abb7db64021f5b3
Kcites-in <kcisclubcoalition@gmail.com>
2024-07-12 07:45:45 +0000

    dda

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./server/db/database');
-const registerRoute = require('./public/js/register');
+const registerRoute = require('./server/routes/register');
 require('dotenv').config();
 
 const app = express();
